7e523c40851c248860f5e120f3c884fc32788ba6
ymmv <ymmv@free.fr>
2011-10-01 10:21:41 +0200

    Move functions into library. Start refactoring pnt_*

---
+++ bin/adar.pm
@@ -0,0 +1,382 @@
+package adar;
+
+use strict;
+use warnings;
+use Carp;
+use Data::Dumper;
+use Exporter qw(import);
+
+our @ISA       = qw(Exporter);
+our @EXPORT = qw( pnt_open bin_open fdt_open fdt_close
+  read_zstring read_lstring read_string read_char
+  read_u8 read_u16le read_u16be read_u32le read_u32be
+  ddm_read pnt_load pnt_dump ddm_dump fdt_dump
+);
+
+our $DEBUG;
+
+sub pnt_open {
+    my $filename = shift @_;
+
+    my $fh;
+    open $fh, '<', $filename or croak "Couldn't open file $filename: $!\n";
+    binmode $fh;
+
+    return $fh;
+}
+
+sub bin_open {
+    my $filename = shift @_;
+
+    my $fh;
+    open $fh, '<', $filename or croak "Couldn't open file $filename: $!\n";
+    binmode $fh;
+
+    return $fh;
+}
+
+sub fdt_open {
+    my $filename = shift @_;
+
+    my $fh;
+    open $fh, '<', $filename or croak "Couldn't open file $filename: $!\n";
+    binmode $fh;
+
+    return $fh;
+}
+
+sub fdt_close {
+    my $fh = shift @_;
+
+    close $fh or croak "Couldn't close file: $!\n";
+}
+
+sub read_zstring {
+    my $fh      = shift @_;
+    my $content = 0;
+
+    my $text = '';
+  ZSTRING:
+    while ( read( $fh, $content, 1 ) ) {
+        my $c = unpack 'C', $content;
+        last ZSTRING if !$c;
+        $text .= $content,;
+    }
+
+    return $text;
+}
+
+sub read_lstring {
+    my $fh = shift @_;
+
+    my $l    = read_u16le($fh);
+    my $text = read_zstring($fh);
+
+    croak "input not an lstring : length($text) != $l"
+      if $l != 1 + length($text);
+    return $text;
+}
+
+sub read_string {
+    my $fh = shift @_;
+
+    my $text = '';
+    while ( read $fh, my $packed_length, 2 ) {
+        my $length = unpack 'v', $packed_length;
+
+        read $fh, $text, $length;
+
+        print STDERR $length, "\t", $text, "\n"
+          if $DEBUG;
+    }
+    return $text;
+}
+
+sub read_char {
+    my $fh = shift @_;
+    read $fh, my $content, 1;
+    return $content;
+}
+
+sub read_u8 {
+    my $fh = shift @_;
+    read $fh, my $content, 1;
+    my $val = unpack 'C', $content;
+    return $val;
+}
+
+sub read_u16le {
+    my $fh = shift @_;
+
+    read $fh, my $content, 2;
+    my $val = unpack 'v', $content;
+
+    return $val;
+}
+
+sub read_u16be {
+    my $fh = shift @_;
+
+    read $fh, my $content, 2;
+    my $val = unpack 'n', $content;
+
+    return $val;
+}
+
+sub read_u32le {
+    my $fh = shift @_;
+
+    read $fh, my $content, 4;
+    my $val = unpack 'V', $content;
+
+    return $val;
+}
+
+sub read_u32be {
+    my $fh = shift @_;
+
+    read $fh, my $content, 4;
+    my $val = unpack 'V', $content;
+
+    return $val;
+}
+
+sub ddm_read {
+    my $filename = shift @_;
+    my %dbdesc;
+
+    if ( !-f $filename ) {
+        if ( -f $filename . '.ddm' ) {
+            $filename = $filename . '.ddm';
+        }
+        else {
+            croak "Can't find ddm file $filename";
+        }
+    }
+
+    my $cfg = Config::IniFiles->new( -file => $filename );
+
+    # FILE* sections
+    my @sections = $cfg->Sections;
+    print 'Sections ' . Dumper \@sections;
+    my @s = grep /^FILE/, @sections;
+    print 'FILE Sections ' . Dumper \@s;
+
+    # Feed the {files} subhash with what is present in the ddm, mainly
+    # fdt file names
+    foreach my $s ( grep /^FILE/, @sections ) {
+        foreach my $p ( $cfg->Parameters($s) ) {
+            $dbdesc{files}->{$s}->{$p} = $cfg->val( $s, $p );
+        }
+    }
+
+    # Implant bin and pnt files in %dbdesc for each fdt.
+    foreach my $fnr ( keys %{ $dbdesc{files} } ) {
+        if ( defined $fnr ) {
+            my $basename = $dbdesc{files}->{$fnr}->{fdt};
+            $basename =~ s/\.fdt$/.bin/;
+            $dbdesc{files}->{$fnr}->{bin_file} = $basename
+              if not defined $dbdesc{files}->{$fnr}->{bin_file};
+            $basename =~ s/\.bin$/.pnt/;
+            $dbdesc{files}->{$fnr}->{pnt_file} = $basename
+              if not defined $dbdesc{files}->{$fnr}->{pnt_file};
+        }
+    }
+
+    # DDF section
+    my $count;
+    foreach my $p ( $cfg->Parameters('DDF') ) {
+        if ( $p eq 'cnt' ) {
+            $count = $cfg->val( 'DDF', $p );
+        }
+        else {
+            my $fieldesc = $cfg->val( 'DDF', $p );
+            my ( $field, $type, $comment );
+            if ( $fieldesc =~ m{ (\w{2}) \s* [,] ([\w]+) \s* (.*) }imxs ) {
+                ( $field, $type, $comment ) = ( $1, $2, $3 );
+                if ( defined $comment ) {
+                    $comment =~ s{ \A \s* [;] \s* }{}imxs;
+                }
+            }
+            $dbdesc{ddm_fields}->{$p}->{name}    = $field;
+            $dbdesc{ddm_fields}->{$p}->{type}    = $type;
+            $dbdesc{ddm_fields}->{$p}->{comment} = $comment if defined $comment;
+            $dbdesc{ddm_fields}->{$field}->{type} = $type;
+        }
+    }
+    print Dumper \%dbdesc if $DEBUG;
+
+    return \%dbdesc;
+}
+
+my %pnt_method = (
+    D => \&read_u32le,
+    K => \&read_u32le,
+    S => \&read_zstring,
+);
+
+sub pnt_load {
+    my $dbdesc = shift @_;
+
+    foreach my $file ( keys %{ $dbdesc->{files} } ) {
+        my $filename = $dbdesc->{files}->{$file}->{pnt_file};
+
+        print "-- Reading pnt file $filename\n" if $DEBUG;
+
+        my $fh = pnt_open($filename);
+
+        while ( !eof($fh) ) {
+            my $key = $pnt_method{ $dbdesc->{ddm_fields}->{1}->{type} }->($fh);
+            my $p   = $pnt_method{ $dbdesc->{ddm_fields}->{2}->{type} }->($fh);
+
+            $dbdesc->{files}->{$file}->{pnt}->[$key] = $p;
+        }
+
+        close $fh or croak "Can't close $filename";
+
+        #        print Dumper $dbdesc->{files}->{$file}->{pnt} if $DEBUG;
+    }
+
+}
+
+sub pnt_dump {
+    my $dbdesc = shift @_;
+
+    foreach my $file ( keys %{ $dbdesc->{files} } ) {
+        print
+          "-- Dumping pnt file $file $dbdesc->{files}⁻>{$file}->{pnt_file}\n";
+
+        if ( defined( $dbdesc->{files}->{$file}->{pnt} ) ) {
+
+            print "-- Index\n";
+            my $deleted    = 0;
+            my $prevoffset = 0;
+            my $maxlen     = 0;
+            my $minlen     = 10000000;
+            foreach
+              my $i ( 0 .. scalar( @{ $dbdesc->{files}->{$file}->{pnt} } ) )
+            {
+                my $p = $dbdesc->{files}->{$file}->{pnt}->[$i];
+
+                if ( defined $p ) {
+                    my $len = $p - $prevoffset;
+                    $maxlen = $len if $len > $maxlen;
+                    $minlen = $len if $len < $minlen && $len != 0;
+                    print "$i : $p ($len)\n";
+
+                    $prevoffset = $p;
+                }
+                else {
+                    $deleted++;
+                    print "$i : deleted\n";
+
+                }
+            }
+            print "minlen : $minlen, maxlen = $maxlen, deleted = $deleted\n";
+        }
+    }
+
+}
+
+sub ddm_dump {
+    my $dbdesc = shift @_;
+
+    print Dumper $dbdesc;
+}
+
+sub fdt_dump {
+    my $dbdesc = shift @_;
+
+    my @fn = map { $dbdesc->{files}->{$_}->{fdt} } keys %{ $dbdesc->{files} };
+
+    foreach my $filename (@fn) {
+        if ( !-f $filename ) {
+            if ( -f $filename . '.fdt' ) {
+                $filename = $filename . '.fdt';
+            }
+        }
+
+        my $fh = fdt_open($filename);
+
+        #
+        # Header
+        #
+
+        # 8 octets
+        #    print read_u8($fh), "\n";
+
+        my $nf;
+        print read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+        print $nf = read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+
+        # Nom de fichier de données
+        print read_zstring($fh), "\n";
+
+        # extension
+        print read_zstring($fh), "\n";
+
+        # Nom de fichier d'index
+        print read_zstring($fh), "\n";
+
+        # extension
+        print read_zstring($fh), "\n";
+
+        print "-- 5 champs 16bit\n";
+        print read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+        print read_u16le($fh), "\n";
+
+        # premier champ
+        print "-- Premier champ ??\n";
+        my $fieldname = read_char($fh) . read_char($fh);
+        print "$filename :",
+          read_u16le($fh), " ",
+          read_u16le($fh), " ",
+          read_u16le($fh), " ",
+          read_u16le($fh), " ",
+          read_u16le($fh), "\n";
+
+        my %field;
+
+        # Description des champs
+        print "-- Description des champs\n";
+        for my $i ( 1 .. $nf ) {
+
+            # nom du champ (zstring ?)
+            my $fieldname = read_zstring($fh);
+            print "$fieldname : ",
+              read_u16le($fh), " ",
+              read_u16le($fh), " ",
+              read_u16le($fh), " ",
+              read_u16le($fh), " ",
+              read_u16le($fh), " ",
+              "\n";
+        }
+
+        # informations sur les champs ou index ??
+        print "-- Informations sur les champs ou index\n";
+        for my $i ( 1 .. $nf ) {
+            print read_u16le($fh),   " ",
+              print read_u16le($fh), " ",
+              print read_u16le($fh), " ",
+              print read_u16le($fh), " ",
+              "\n";
+        }
+
+        print read_u16le($fh), "\n";
+
+        # Description des colonnes
+        print "-- Description des colonnes\n";
+        foreach my $i ( 1 .. $nf ) {
+            print read_lstring($fh), "\n";
+        }
+    }
+
+}
+
+1;
+
